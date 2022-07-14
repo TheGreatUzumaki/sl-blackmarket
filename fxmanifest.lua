09a77a88d813a418546775e7de5d451c5d05cf4c
TheGreatUzumaki <109258057+TheGreatUzumaki@users.noreply.github.com>
2022-07-13 22:30:46 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sideload#1415'
+description 'sl-blackmarket: Blackmarket Plugin'
+version '1.0BETA'
+
+shared_script 'config.lua'
+
+client_script 'client/main.lua'
+
+server_script 'server/main-server.lua'
